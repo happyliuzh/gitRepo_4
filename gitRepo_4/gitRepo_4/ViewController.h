a643c81485e6522967744df6cbfa7760b03876f3
liuzhihui <liuzhihui@postop.cn>
2016-06-01 16:30:01 +0800

    first proj

---
+++ gitRepo_4/gitRepo_4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitRepo_4
+//
+//  Created by 刘志会 on 16/6/1.
+//  Copyright © 2016年 lzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitRepo_4/gitRepo_4/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitRepo_4
+//
+//  Created by 刘志会 on 16/6/1.
+//  Copyright © 2016年 lzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
