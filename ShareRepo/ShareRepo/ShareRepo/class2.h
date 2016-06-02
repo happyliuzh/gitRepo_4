0965813dfcfbb520643d38289a7cdba2680ac998
liuzhihui <liuzhihui@postop.cn>
2016-06-02 09:05:14 +0800

    fix

---
+++ ShareRepo/ShareRepo/ShareRepo/class2.h
@@ -0,0 +1,13 @@
+//
+//  class2.h
+//  ShareRepo
+//
+//  Created by 刘志会 on 16/6/2.
+//  Copyright © 2016年 lzh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface class2 : NSObject
+
+@end
