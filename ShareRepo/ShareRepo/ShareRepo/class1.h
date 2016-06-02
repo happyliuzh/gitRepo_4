e59f8bb1090f15e2a4c40cbbb68e057224deec7c
liuzhihui <liuzhihui@postop.cn>
2016-06-02 08:59:31 +0800

    add class1

---
+++ ShareRepo/ShareRepo/ShareRepo/class1.h
@@ -0,0 +1,13 @@
+//
+//  class1.h
+//  ShareRepo
+//
+//  Created by 刘志会 on 16/6/2.
+//  Copyright © 2016年 lzh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface class1 : NSObject
+
+@end
---
+++ ShareRepo/ShareRepo/ShareRepo/class1.m
@@ -0,0 +1,13 @@
+//
+//  class1.m
+//  ShareRepo
+//
+//  Created by 刘志会 on 16/6/2.
+//  Copyright © 2016年 lzh. All rights reserved.
+//
+
+#import "class1.h"
+
+@implementation class1
+
+@end
